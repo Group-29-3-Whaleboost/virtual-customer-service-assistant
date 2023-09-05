60d2913ad2517279937443e227da6ffbd75bc9d3
Kavishka111Santhush <kavishkasanthush201@gmail.com>
2023-09-05 23:50:29 +0530

    Refactored the End user Dashboard

---
+++ application/views/includes/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title><?php echo $title ?></title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php base_url(); ?>assets\css\bootstrap.min.css"/>
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
+
+</head>
