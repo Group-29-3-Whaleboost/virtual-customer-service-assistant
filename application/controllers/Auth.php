5a986139ff6d737e8b1047ffd1b39050696423cb
Lahiru Madhuka <lahirumadhuka4060@gmail.com>
2023-09-05 19:23:43 +0530

    I fixed some mistakes and put url of google drive images and changed the path of user-login-&-registration

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -9,7 +9,7 @@
 
         public function login_registration(){
             $data['title'] = 'Login & Registration | CAS';
-            $this->load->view('enduser/user-login-&-registration/user-login-&-registration',$data);
+            $this->load->view('enduser/user-login-&-registration',$data);
            
         }
         
