44ec621413552e79d7d30e7f0415ad80dbf7afed
Dinidu Atapattu <atapattudinidu@gmail.com>
2023-09-11 12:37:50 +0530

    I change footer and header file in include folder I create Main.php controler. I create and change main-page.php and also I put a new style css file named main.css and add some new images into assets folder

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -4,11 +4,12 @@
 <head>
 
     <title><?php echo $title ?></title>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php base_url(); ?>assets\css\bootstrap.min.css"/>
-
+   
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet">
 
 </head>
